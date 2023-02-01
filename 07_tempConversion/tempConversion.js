cc4856587002e03a3425be6dff55ac14424361e3
midsommarnatt <manne@vardag.org>
2023-02-01 15:12:03 +0100

    Finish seventh exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let convertedTemperature = (temperature - 32) / 1.8;
+  return Math.round(convertedTemperature * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let convertedTemperature = temperature * 1.8 + 32;
+  return Math.round(convertedTemperature * 10) / 10;
 };
 
 // Do not edit below this line
