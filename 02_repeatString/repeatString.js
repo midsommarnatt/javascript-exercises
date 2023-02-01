7b26184e3cfb24944df84de39161f026bc4e69b0
midsommarnatt <manne@vardag.org>
2023-02-01 14:24:21 +0100

    Finish second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
 
+    let newString = '';
+    for (let index = 0; index < num; index++) {
+        newString += str;
+    }
+    return newString;
 };
 
 // Do not edit below this line
