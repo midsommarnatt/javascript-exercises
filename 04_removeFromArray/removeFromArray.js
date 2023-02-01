8c79d59e8e70f9364734fb6d7067b73605079839
midsommarnatt <manne@vardag.org>
2023-02-01 14:44:48 +0100

    Finish fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    for (const arg of args) {
+        for (let index = 0; index < arr.length; index++) {
+            if (arr[index] === arg) {
+                arr.splice(index, 1);
+            } 
+        }       
+    }
+    return arr;
 };
 
 // Do not edit below this line
