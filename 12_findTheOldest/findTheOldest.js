aea965fa605a84a6c0b98bead580ea5d7b6bd460
midsommarnatt <manne@vardag.org>
2023-02-04 17:28:09 +0100

    Finish twelfth exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    const oldest = people.sort((a, b) => {
+        const currentYear = new Date().getFullYear();
+        const lastGuy = (('yearOfDeath' in a ? a.yearOfDeath : currentYear) - a.yearOfBirth);
+        const nextGuy = (('yearOfDeath' in b ? b.yearOfDeath : currentYear) - b.yearOfBirth);
+        return lastGuy > nextGuy ? -1 : 1;
+    })[0];
+    return oldest;
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
