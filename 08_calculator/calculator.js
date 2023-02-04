3f1fe210ecd008b2d641aa25bb033650f4358708
midsommarnatt <manne@vardag.org>
2023-02-04 15:39:01 +0100

    Finish eigth exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(...args) {
+	let total = 0;
+  for (const arg of args) {
+    total += arg;
+  }
+  return total;
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+	return args[0] - args[1];
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let total = 0;
+	for (const i of arr) {
+    total += i;
+  };
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    total *= arr[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (!num) {
+    return 1;
+  }
+  total = num;
+	for (let i = num - 1; i > 0; i--) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
