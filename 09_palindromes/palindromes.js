8a538615935b8eeb7b33b73837067d383bbd6edf
midsommarnatt <manne@vardag.org>
2023-02-04 16:14:22 +0100

    Finish ninth exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = Array.from(str);
+    const newStr = str
+        .filter(char => char.toLowerCase() != char.toUpperCase())
+        .map(char => char.toLowerCase());
 
+    for (let i = 0; i < newStr.length; i++) {
+        if (newStr[i] != newStr[newStr.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
