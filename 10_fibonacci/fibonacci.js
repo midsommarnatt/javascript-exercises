42b03ed45d431e85d997c059a729063b6a3b601a
midsommarnatt <manne@vardag.org>
2023-02-04 16:28:31 +0100

    Finish tenth exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let num = parseInt(number);
+    if (num < 0) {return 'OOPS'};
+    let sequence = [1];
 
+    for (let i = 0; i < num; i++) {
+        if (sequence.length == 1) {
+            sequence.push(sequence[i] + 0);
+            continue;
+        }
+        sequence.push(sequence[i] + sequence[i - 1]);
+    }
+    return sequence[num - 1];
 };
 
 // Do not edit below this line
